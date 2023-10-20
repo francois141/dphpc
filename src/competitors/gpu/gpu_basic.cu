97717b35d500f2dd818d96191705bf0162821912
François Costa <frankost.costa@gmail.com>
2023-10-20 10:06:21 +0200

    Replace assertion by static_assert

--- src/competitors/gpu/gpu_basic.cu
+++ src/competitors/gpu/gpu_basic.cu
@@ -53,8 +53,7 @@ void gpu_basic_coo_wrapper(Dense<T>& A, Dense<T>& B, COO<T>& S, COO<T>& P) {
 	size_t SP_size = S.getValues().size() * sizeof(T);
 	size_t sparse_dim_size = S.getValues().size() * sizeof(int);
 
-	// the kernel is specialized for double precision floating points
-	assert(sizeof(T) == sizeof(double));
+    static_assert(sizeof(T) == sizeof(double), "the kernel is specialized for double precision floating points");
 
 	// allocate the matrices on the GPU
 	double* A_gpu, * B_gpu, * S_gpu, * P_gpu;
@@ -89,11 +88,6 @@ void gpu_basic_coo_wrapper(Dense<T>& A, Dense<T>& B, COO<T>& S, COO<T>& P) {
 }
 
 /* Workaround because the wrappers need to be inside the CUDA file (Would normally write templated functions inside the header file!) */
-template void gpu_basic_csr_wrapper<int>(Dense<int> &A, Dense<int> &B, CSR<int> &S, CSR<int> &P);
-template void gpu_basic_coo_wrapper<int>(Dense<int> &A, Dense<int> &B, COO<int> &S, COO<int> &P);
-
-template void gpu_basic_csr_wrapper<float>(Dense<float> &A, Dense<float> &B, CSR<float> &S, CSR<float> &P);
-template void gpu_basic_coo_wrapper<float>(Dense<float> &A, Dense<float> &B, COO<float> &S, COO<float> &P);
 
 template void gpu_basic_csr_wrapper<double>(Dense<double>& A, Dense<double>& B, CSR<double>& S, CSR<double>& P);
 template void gpu_basic_coo_wrapper<double>(Dense<double>& A, Dense<double>& B, COO<double>& S, COO<double>& P);
