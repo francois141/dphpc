71455b3974186d4cab866baacedf29bf7ecf17ad
Macdu <Macdu@users.noreply.github.com>
2023-10-25 18:13:04 +0200

    Fix gpu pytorch implementation

--- src/competitors/gpu/gpu_pytorch.hpp
+++ src/competitors/gpu/gpu_pytorch.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <torch/torch.h>
+
 #include "benchmark/competitor.hpp"
 #include "matrices/matrices.h"
 
@@ -29,25 +31,27 @@ namespace Competitors {
                 const unsigned int B_col = B.getCols();
                 const unsigned int B_row = B.getRows();
                 
-                // create tensors out of flat vector, by setting (row_stride, col_stride) to (num_cols, 1) on GPU
-                at::TensorOptions options_scalar = torch::device(torch::kCUDA).dtype(scalar_type);
-                torch::Tensor A_tensor = torch::from_blob(A.get_pointer(), {A_row, A_col}, {A_col, 1}, options_scalar);
-                torch::Tensor B_tensor = at::transpose(torch::from_blob(B.get_pointer(), {B_row, B_col}, {B_col, 1}, options_scalar), 0, 1);
+                const auto cpu = torch::device(torch::kCPU);
+                const auto gpu = torch::device(torch::kCUDA);
+
+                // create tensors out of flat vector, by setting (row_stride, col_stride) to (num_cols, 1) on CPU, then copy them to the GPU
+                torch::Tensor A_tensor = torch::from_blob(A.get_pointer(), { A_row, A_col }, { A_col, 1 }, scalar_type).to(gpu);
+                torch::Tensor B_tensor = at::transpose(torch::from_blob(B.get_pointer(), {B_row, B_col}, {B_col, 1}, scalar_type), 0, 1).to(gpu);
 
                 
                 // create sparse CSR tensor for CPU
-                at::TensorOptions options_int = torch::device(torch::kCUDA).dtype(torch::kInt);
-                torch::Tensor crow_indices = torch::tensor(S.getRowPositions(), options_int);
-                torch::Tensor col_indices = torch::tensor(S.getColPositions(), options_int);
-                torch::Tensor values = torch::tensor(S.getValues(), options_scalar);
+                torch::ScalarType int_type = torch::kInt;
+                torch::Tensor crow_indices = torch::tensor(S.getRowPositions(), int_type);
+                torch::Tensor col_indices = torch::tensor(S.getColPositions(), int_type);
+                torch::Tensor values = torch::tensor(S.getValues(), scalar_type);
                 torch::IntArrayRef size = {S.getRows(), S.getCols()};
-                torch::Tensor sparse_tensor = torch::sparse_csr_tensor(crow_indices, col_indices, values, size, options_scalar);
+                torch::Tensor sparse_tensor = torch::sparse_csr_tensor(crow_indices, col_indices, values, size, scalar_type).to(gpu);
 
-                torch::Tensor result = at::native::sparse_sampled_addmm_sparse_csr_cuda(sparse_tensor, A_tensor, B_tensor, 0, 1);
+                torch::Tensor result = at::native::sparse_sampled_addmm_sparse_csr_cuda(sparse_tensor, A_tensor, B_tensor, 0, 1).to(cpu);
 
-                P.setRowPositions(std::vector<int>(result.crow_indices().data_ptr<int64_t>(), result.crow_indices().data_ptr<int64_t>() + result.crow_indices().numel()));
-                P.setColPositions(std::vector<int>(result.col_indices().data_ptr<int64_t>(), result.col_indices().data_ptr<int64_t>() + result.col_indices().numel()));
-                P.setValues(std::vector<T>(result.values().data_ptr<T>(), result.values().data_ptr<T>() + result.values().numel()));
+                P.setRowPositions(std::vector<int>(result.crow_indices().const_data_ptr<int64_t>(), result.crow_indices().const_data_ptr<int64_t>() + result.crow_indices().numel()));
+                P.setColPositions(std::vector<int>(result.col_indices().const_data_ptr<int64_t>(), result.col_indices().const_data_ptr<int64_t>() + result.col_indices().numel()));
+                P.setValues(std::vector<T>(result.values().const_data_ptr<T>(), result.values().const_data_ptr<T>() + result.values().numel()));
             }
 
             virtual inline void run_coo(Dense<T> &A, Dense<T> &B, COO<T> &S, COO<T> &P) {
