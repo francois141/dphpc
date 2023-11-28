61f5ff3edf310b52c591bd1c04f3e2468fc8760a
François Costa <frankost.costa@gmail.com>
2023-11-28 14:25:24 +0100

    Revert "Fixed == operator in CSR.hpp"
    This reverts commit 96360c9c

--- src/matrices/CSR.hpp
+++ src/matrices/CSR.hpp
@@ -9,7 +9,6 @@
 #include <ostream>
 #include <vector>
 #include <algorithm>
-#include <map>
 #include <numeric>
 
 #include "triplet.h"
@@ -69,42 +68,29 @@ public:
     }
 
     friend bool operator==(const CSR &lhs, const CSR &rhs) {
-        bool posMatch = lhs.colPositions.size() == rhs.colPositions.size() &&
-                        lhs.rowPositions.size() == rhs.rowPositions.size() &&
-                        lhs.rows == rhs.rows &&
-                        lhs.cols == rhs.cols;
+        bool posMatch = lhs.colPositions == rhs.colPositions &&
+               lhs.rowPositions == rhs.rowPositions &&
+               lhs.rows == rhs.rows &&
+               lhs.cols == rhs.cols;
 
         if(!posMatch) {
             std::cout << "Cols and Rows don't match" << std::endl;
-            return false;
-        }
-
-        std::map<std::pair<int,int>,T> mp;
-
-        for(int row_idx = 0; row_idx < lhs.rows - 1;row_idx++) {
-            int col_idx = lhs.rowPositions[row_idx];
-            while(col_idx < lhs.rowPositions[row_idx+1]) {
-                mp.insert(std::make_pair(std::make_pair(lhs.colPositions[col_idx], row_idx), lhs.values[col_idx]));
-                col_idx++;
-            }
         }
 
         bool valMatch = true;
         T largestDiff = 0;
 
-        for(int row_idx = 0; row_idx < lhs.rows - 1;row_idx++) {
-            int col_idx = rhs.rowPositions[row_idx];
-            while(col_idx < rhs.rowPositions[row_idx+1]) {
-                std::pair<int,int> coord = std::make_pair(lhs.colPositions[col_idx], row_idx);
+        if(std::is_integral<T>::value) {
+            valMatch = (lhs.values == rhs.values);
+        } else {
+            if(lhs.values.size() != rhs.values.size()) {
+                return false;
+            }
 
-                if(mp.find(coord) == mp.end()) {
-                    return false;
-                }
-
-                T diff = (std::abs(rhs.values[col_idx] - mp[coord]) / rhs.values[col_idx]);
+            for(uint32_t i = 0; i < lhs.values.size();i++) {
+                T diff = (std::abs(rhs.values[i] - lhs.values[i]) / rhs.values[i]);
                 largestDiff = std::max(largestDiff, diff);
                 valMatch &= diff <= 1e-6;
-                col_idx++;
             }
         }
 
@@ -112,7 +98,7 @@ public:
             std::cout << "Largest relative difference is : " << largestDiff << std::endl;
         }
 
-        return valMatch;
+        return posMatch && valMatch;
     }
 
     int getRows() const {
