d9d610b47ec01531e65c25a0366199a9297f0c6a
Paul Elvinger <elpaul@Pauls-MacBook-Pro.local>
2023-12-17 10:11:00 +0100

    fix bug

--- src/benchmark/benchmark.hpp
+++ src/benchmark/benchmark.hpp
@@ -70,32 +70,32 @@ namespace SDDMM {
                     /* ============================= */
                     if (competitor->csr_supported()) {
                         DEBUG_OUT("Running competitor " << competitor->name << " (Sparse matrices represented as CSR) " << num_runs << " times" << std::endl);
-                            for (int i = 0; i < num_runs; i++){
-                                CSR<T> P_csr(this->getDataset().getS_CSR());
-                                P_csr.clearValues();
+                        
+                        for (int i = 0; i < num_runs; i++){
+                            CSR<T> P_csr(this->getDataset().getS_CSR());
+                            P_csr.clearValues();
 
-                                // Running competitor
-                                SDDMM::timing_result res = timing(
-                                    competitor->is_gpu(),
-                                    [&] { competitor->init_csr(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_CSR(), P_csr); },
-                                    [&] { competitor->run_csr(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_CSR(), P_csr); },
-                                    [&] { competitor->cleanup_csr(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_CSR(), P_csr); }
-                                );
-                                
-                                // Checking correctness if available
-                                if (this->getDataset().hasExpected()) {
-                                    csr_correctness = (P_csr == this->getDataset().getExpected_CSR());
-                                    if (!csr_correctness) {
-                                        //DEBUG_OUT(" - !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
-                                        //FILE_DUMP("[ " << competitor->name << "] !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
-                                    }
+                            // Running competitor
+                            SDDMM::timing_result res = timing(
+                                competitor->is_gpu(),
+                                [&] { competitor->init_csr(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_CSR(), P_csr); },
+                                [&] { competitor->run_csr(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_CSR(), P_csr); },
+                                [&] { competitor->cleanup_csr(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_CSR(), P_csr); }
+                            );
+                            
+                            // Checking correctness if available
+                            if (this->getDataset().hasExpected()) {
+                                csr_correctness = (P_csr == this->getDataset().getExpected_CSR());
+                                if (!csr_correctness) {
+                                    //DEBUG_OUT(" - !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
+                                    //FILE_DUMP("[ " << competitor->name << "] !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
                                 }
-                                DEBUG_OUT(" - Execution took " << MILLISECOND(res.comp_ns) << " milliseconds (" << res.comp_ns << "ns)" << std::endl << std::endl);
-                                FILE_DUMP(competitor->name << "," << this->getDataset().getName() << ",CSR,"
-                                    << this->getDataset().getS_COO().getRows() << "," << this->getDataset().getS_COO().getCols() << "," << this->getDataset().getA().getCols() << "," << this->getDataset().getS_COO().getValues().size() << ","
-                                    << res.total_ns << "," << res.init_ns << "," << res.comp_ns << "," << res.cleanup_ns << "," << csr_correctness << "," << competitor->get_num_thread_blocks() << "," << competitor->get_num_threads_per_block() <<  std::endl
-                                );
                             }
+                            DEBUG_OUT(" - Execution took " << MILLISECOND(res.comp_ns) << " milliseconds (" << res.comp_ns << "ns)" << std::endl << std::endl);
+                            FILE_DUMP(competitor->name << "," << this->getDataset().getName() << ",CSR,"
+                                << this->getDataset().getS_COO().getRows() << "," << this->getDataset().getS_COO().getCols() << "," << this->getDataset().getA().getCols() << "," << this->getDataset().getS_COO().getValues().size() << ","
+                                << res.total_ns << "," << res.init_ns << "," << res.comp_ns << "," << res.cleanup_ns << "," << csr_correctness << "," << competitor->get_num_thread_blocks() << "," << competitor->get_num_threads_per_block() <<  std::endl
+                            );
                         }
 
                     }
@@ -106,34 +106,33 @@ namespace SDDMM {
                     if (competitor->coo_supported()) {
                         DEBUG_OUT("Running competitor " << competitor->name << " (Sparse matrices represented as COO) " << num_runs << " times" << std::endl);
                             
-                            for (int i = 0; i < num_runs; i++){
-                                COO<T> P_coo(this->getDataset().getS_COO());
-                                P_coo.clearValues();
+                        for (int i = 0; i < num_runs; i++){
+                            COO<T> P_coo(this->getDataset().getS_COO());
+                            P_coo.clearValues();
 
-                                // Running competitor
-                                SDDMM::timing_result res = timing(
-                                    competitor->is_gpu(),
-                                    [&] { competitor->init_coo(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_COO(), P_coo); },
-                                    [&] { competitor->run_coo(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_COO(), P_coo); },
-                                    [&] { competitor->cleanup_coo(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_COO(), P_coo); }
-                                );
+                            // Running competitor
+                            SDDMM::timing_result res = timing(
+                                competitor->is_gpu(),
+                                [&] { competitor->init_coo(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_COO(), P_coo); },
+                                [&] { competitor->run_coo(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_COO(), P_coo); },
+                                [&] { competitor->cleanup_coo(this->getDataset().getA(), this->getDataset().getB(), this->getDataset().getS_COO(), P_coo); }
+                            );
 
-                                // Checking correctness if available
-                                if (competitor->coo_supported() && this->getDataset().hasExpected()) {
-                                    coo_correcntess = (P_coo == this->getDataset().getExpected_COO());
-                                    if (!coo_correcntess) {
-                                        //DEBUG_OUT(" - !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
-                                        //FILE_DUMP("[ " << competitor->name << "] !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
-                                    }
+                            // Checking correctness if available
+                            if (competitor->coo_supported() && this->getDataset().hasExpected()) {
+                                coo_correcntess = (P_coo == this->getDataset().getExpected_COO());
+                                if (!coo_correcntess) {
+                                    //DEBUG_OUT(" - !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
+                                    //FILE_DUMP("[ " << competitor->name << "] !!! Wrong results calculated compared to CPU-Basic (CSR) !!!" << std::endl);
                                 }
-                                DEBUG_OUT(" - Execution took " << MILLISECOND(res.comp_ns) << " milliseconds - (" << res.comp_ns << "ns)" << std::endl << std::endl);
-                                FILE_DUMP(competitor->name << "," << this->getDataset().getName() << ",COO,"
-                                    << this->getDataset().getS_COO().getRows() << "," << this->getDataset().getS_COO().getCols() << "," << this->getDataset().getA().getCols() << "," << this->getDataset().getS_COO().getValues().size() << ","
-                                    << res.total_ns << "," << res.init_ns << "," << res.comp_ns << "," << res.cleanup_ns << "," << coo_correcntess << "," << competitor->get_num_thread_blocks() << "," << competitor->get_num_threads_per_block() << std::endl
-                                );
                             }
-                            
+                            DEBUG_OUT(" - Execution took " << MILLISECOND(res.comp_ns) << " milliseconds - (" << res.comp_ns << "ns)" << std::endl << std::endl);
+                            FILE_DUMP(competitor->name << "," << this->getDataset().getName() << ",COO,"
+                                << this->getDataset().getS_COO().getRows() << "," << this->getDataset().getS_COO().getCols() << "," << this->getDataset().getA().getCols() << "," << this->getDataset().getS_COO().getValues().size() << ","
+                                << res.total_ns << "," << res.init_ns << "," << res.comp_ns << "," << res.cleanup_ns << "," << coo_correcntess << "," << competitor->get_num_thread_blocks() << "," << competitor->get_num_threads_per_block() << std::endl
+                            );
                         }
+
                     }
                 });
             }
