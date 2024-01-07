5cd1df3e4a33d44491c8cf4e47d20cbfca22c354
Paul Elvinger <elvingerpa@gmail.com>
2024-01-07 20:06:27 +0100

    use seaborn theme

--- plots/motivation_figure.py
+++ plots/motivation_figure.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 val = pd.read_csv("results/motivation/v100/results-v100.csv")
 val = val[['competitor', 'dataset', 'comp_ns']]
@@ -8,9 +9,8 @@ val = val.groupby(by= ["competitor", "dataset"]).mean()
 val['comp_ns'] /= 1000
 print(val)
 
-# Create some fake data.
-
-
+sns.set_theme(context="notebook", font_scale=1, style="darkgrid", rc={ "lines.linewidth": 2, "axes.linewidth": 1, "axes.edgecolor":"black", "xtick.bottom": True, "ytick.left": True })
+sns.despine(left=True, bottom=False)
 
 y = [0.1,0.01,0.001,0.0001,0.00001]
 
@@ -28,21 +28,20 @@ fig, (ax1, ax2) = plt.subplots(2, 1)
 ax1.plot(y, x2, '.-', label="Tiling")
 ax1.plot(y, x1, '.-', label="No tiling")
 ax1.set_xscale('log')
-ax1.set_ylabel('A100 - milliseconds')
+ax1.set_yscale('log')
+ax1.set_ylabel('A100 runtime [ms]')
 ax1.legend(loc="upper right")
 ax1.invert_xaxis()
-ax1.set_facecolor('#f5f5f5')
 #ax1.set_ylim([0, 1600])
 
 ax2.plot(y, v100_2, '.-', label="Tiling")
 ax2.plot(y, v100_1, '.-', label="No Tiling")
 ax2.set_xscale('log')
-ax2.set_ylabel('V100 - milliseconds')
-ax2.set_xlabel('Density')
+ax2.set_yscale('log')
+ax2.set_ylabel('V100 runtime [ms]')
+ax2.set_xlabel('Density (%)')
 ax2.legend(loc="upper right")
 ax2.invert_xaxis()
-ax2.set_facecolor('#f5f5f5')
 #ax2.set_ylim([0, 1600])
-
 plt.savefig("output.png", dpi = 300)
 plt.show()
